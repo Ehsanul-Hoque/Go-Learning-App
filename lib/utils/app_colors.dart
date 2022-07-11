d97d1e89947b00c4e1ce91fc6058723f958635ec
Ehsanul-Hoque <ehsanfahad01@gmail.com>
2022-07-11 23:44:08 +0600

    AppColors and AppParameters created. main.dart updated.

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color black = Colors.black;
+  static const Color white = Colors.white;
+  static const Color transparent = Colors.transparent;
+
+  static const Color themeBlue = Color(0xff28295B);
+  static const Color themeYellow = Color(0xffF8AF3E);
+  static const Color veryLightGrey = Color(0xffF5F5F5);
+}
---
+++ lib/utils/app_parameters.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class AppParameters {
+  static const double defaultBottomNavBarHeight = 56;
+  static const double defaultBottomNavBarContentMinWidth = 56;
+  static const Duration defaultAnimationDuration = Duration(milliseconds: 200);
+  static const Curve defaultAnimationCurve = Curves.easeInOut;
+  static const TextStyle defaultTextStyle = TextStyle(
+    fontSize: 14,
+    fontFamily: "Poppins",
+  );
+}
