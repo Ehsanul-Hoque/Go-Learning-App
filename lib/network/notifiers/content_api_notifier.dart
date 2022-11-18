70c8f584b5788428da3f2b3af37f19ecf64b0f90
Ehsanul-Hoque <ehsanfahad01@gmail.com>
2022-11-19 03:28:31 +0600

    Lecture response methods added in ContentApiNotifier.

--- lib/network/notifiers/content_api_notifier.dart
+++ lib/network/notifiers/content_api_notifier.dart
@@ -2,11 +2,14 @@
 
 import "package:app/network/converters/default_converters/json_object_converter.dart";
 import "package:app/network/models/api_contents/content_tree_get_response_model.dart";
+import "package:app/network/models/api_contents/lecture_get_response_model.dart";
+import "package:app/network/models/base_api_response_model.dart";
 import "package:app/network/network.dart";
 import "package:app/network/network_request.dart";
 import "package:app/network/network_response.dart";
 import "package:app/network/notifiers/api_notifier.dart";
 import "package:app/network/serializers/api_contents/content_tree_get_response_serializer.dart";
+import "package:app/network/serializers/api_contents/lecture_get_response_serializer.dart";
 import "package:app/network/serializers/base_api_response_serializer.dart";
 import "package:flutter/widgets.dart" show BuildContext;
 import "package:provider/provider.dart" show ChangeNotifierProvider;
@@ -22,6 +25,12 @@ class ContentApiNotifier extends ApiNotifier {
             : "/full_course/get_full_course_tree?course_id=$courseId");
   }
 
+  static String lectureGetApiEndpoint(String? lectureId) {
+    return (lectureId == null)
+        ? "/lecture/get"
+        : (lectureId.isEmpty ? "/lecture/get" : "/lecture/get?_id=$lectureId");
+  }
+
   /// Constructor
   ContentApiNotifier();
 
@@ -31,7 +40,7 @@ class ContentApiNotifier extends ApiNotifier {
         create: (BuildContext context) => ContentApiNotifier(),
       );
 
-  /// Methods to get coupon
+  /// Methods to get whole content tree of a course
   Future<NetworkResponse<ContentTreeGetResponseModel>> getContentTree(
     String? courseId,
   ) {
@@ -53,6 +62,29 @@ class ContentApiNotifier extends ApiNotifier {
   ) {
     return Network.getOrCreateResponse(
       defaultClient.baseUrl + contentsTreeGetApiEndpoint(courseId),
+    );
+  }
+
+  /// Methods to get a lecture (a type of content) of a course
+  Future<NetworkResponse<BaseApiResponseModel<LectureGetResponseModel>>>
+      getLecture(String? lectureId) {
+    return Network(updateListener: () => notifyListeners()).createExecuteCall(
+      client: defaultClient,
+      request: NetworkRequest.get(
+        apiEndPoint: lectureGetApiEndpoint(lectureId),
+        serializer: const BaseApiResponseSerializer<LectureGetResponseModel>(
+          LectureGetResponseSerializer(),
+        ),
+        converter: const JsonObjectConverter<
+            BaseApiResponseModel<LectureGetResponseModel>>(),
+      ),
+    );
+  }
+
+  NetworkResponse<BaseApiResponseModel<LectureGetResponseModel>>
+      lectureGetResponse(String? lectureId) {
+    return Network.getOrCreateResponse(
+      defaultClient.baseUrl + lectureGetApiEndpoint(lectureId),
     );
   }
 }
