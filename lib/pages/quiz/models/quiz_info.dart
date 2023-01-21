b0b7ef1a26ccc384112a50318e04d5c29ef0295c
Ehsanul-Hoque <ehsanfahad01@gmail.com>
2023-01-22 00:57:21 +0600

    QuizInfo bug fixed and restructured.

--- lib/pages/quiz/models/quiz_info.dart
+++ lib/pages/quiz/models/quiz_info.dart
@@ -2,22 +2,15 @@ import "package:app/pages/quiz/constants/quiz_constants.dart";
 import "package:app/pages/quiz/models/ques_ans_info.dart";
 
 class QuizInfo {
-  QuizInfo({
-    this.quesAns = const <String, QuesAnsInfo>{},
-    this.totalDuration,
-    double? positiveMarks,
-    double? negativeMarks,
-    double? neutralMarks,
-  })  : _positiveMarks = positiveMarks,
-        _negativeMarks = negativeMarks,
-        _neutralMarks = neutralMarks;
-
-  final Map<String, QuesAnsInfo> quesAns;
+  // Fields
   Duration? totalDuration;
+  Map<String, QuesAnsInfo>? _quesAns;
   double? _positiveMarks;
   double? _negativeMarks;
   double? _neutralMarks;
 
+  // Getters
+  Map<String, QuesAnsInfo> get quesAns => _quesAns ?? <String, QuesAnsInfo>{};
   double get positiveMarks =>
       _positiveMarks ?? QuizConstants.defaultPositiveMarksPerAns;
   double get negativeMarks =>
@@ -25,6 +18,8 @@ class QuizInfo {
   double get neutralMarks =>
       _neutralMarks ?? QuizConstants.defaultMarksPerBlankAns;
 
+  // Setters
+  set quesAns(Map<String, QuesAnsInfo>? quesAns) => _quesAns = quesAns;
   set positiveMarks(double? positiveMarks) => _positiveMarks = positiveMarks;
   set negativeMarks(double? negativeMarks) => _negativeMarks = negativeMarks;
   set neutralMarks(double? neutralMarks) => _neutralMarks = neutralMarks;
